0ee89cd8ee8ede8bdd1c684209cef64a22c4f442
Ignatov.AI <ignatov-ai@yandex.ru>
2023-11-22 15:09:52 +0300

    Перестройка проекта под OpenPyXL
    Проверяются все листы книги

--- main_openpyxl.py
+++ main_openpyxl.py
@@ -8,7 +8,7 @@ marks_str = ['2', '3', '4', '5']
 marks_int = [2, 3, 4, 5]
 
 # выбирается файл для обработки
-file = '11 Я тест'
+file = '11 Я'
 book = load_workbook(file + '.xlsx')
 
 # получаем список листов
@@ -17,81 +17,82 @@ print(tabs)
 
 
 # выбирается лист для работы
-sheets = book.active
+for sheet_name in tabs:
+    sheets = book[sheet_name]
 
-# определяем предмет
-lesson = sheets['U41'].value.split(',')[-1].lstrip().rstrip()
+    # определяем предмет
+    lesson = sheets['U41'].value.split(',')[-1].lstrip().rstrip()
 
-# определяем учителя
-teacher_split = sheets['U43'].value.split()[1:4]
-teacher = ' '.join(teacher_split).lstrip().rstrip()
+    # определяем учителя
+    teacher_split = sheets['U43'].value.split()[1:4]
+    teacher = ' '.join(teacher_split).lstrip().rstrip()
 
-# удаляем столбцы с ДЗ
-sheets.delete_cols(20, 23)
+    # удаляем столбцы с ДЗ
+    sheets.delete_cols(20, 23)
 
-# получаем список объединенных диапазонов и разъединяем объединенные ячейки
-merged_cells = list(map(str, sheets.merged_cells.ranges))
+    # получаем список объединенных диапазонов и разъединяем объединенные ячейки
+    merged_cells = list(map(str, sheets.merged_cells.ranges))
 
-for item in merged_cells:
-    try:
-        sheets.unmerge_cells(item)
-    except KeyError:
-        continue
+    for item in merged_cells:
+        try:
+            sheets.unmerge_cells(item)
+        except KeyError:
+            continue
 
-# перемещаем куски оценок в одну общую часть
-for part in range(1, 6):
-    m_range = 'C'+str(1+part*50)+':S'+str((part+1)*50)
-    sheets.move_range(m_range, rows=-50*part, cols=17*part)
+    # перемещаем куски оценок в одну общую часть
+    for part in range(1, 6):
+        m_range = 'C'+str(1+part*50)+':S'+str((part+1)*50)
+        sheets.move_range(m_range, rows=-50*part, cols=17*part)
 
+    # считаем количество учеников на листе
+    students_count = 1
+    while sheets['A'+str(students_count)].value != '':
+        students_count += 1
 
+    # удаляем пустые строки
+    sheets.delete_rows(students_count, sheets.max_row)
 
-# считаем количество учеников на листе
-students_count = 1
-while sheets['A'+str(students_count)].value != '':
-    students_count += 1
+    # зменяем ширину всех столбцов с оценками
+    for i in range(3, int(sheets.max_column)):
+        sheets.column_dimensions[get_column_letter(i)].width = 2
 
-# удаляем пустые строки
-sheets.delete_rows(students_count, sheets.max_row)
+    # меняем тип ячейки на число, если это возможно
+    for row in range(students_count-3):
+        for col in range(3, int(sheets.max_column)):
+            ch_range = get_column_letter(col) + str(row+3)
+            if sheets[ch_range].value in marks_str:
+                sheets[ch_range].value = int(sheets[ch_range].value)
 
-# зменяем ширину всех столбцов с оценками
-for i in range(3, int(sheets.max_column)):
-    sheets.column_dimensions[get_column_letter(i)].width = 2
+    # добавляем колонку КОЛИЧЕСТВО ОЦЕНОК и КОЛИЧЕСТВО ПРОПУСКОВ и подсчитываем количество оценок и пропусков по каждому учащемуся
+    sheets['CJ2'].value = 'Количество оценок'
+    sheets.column_dimensions['CJ'].width = 11
 
-# меняем тип ячейки на число, если это возможно
-for row in range(students_count-3):
-    for col in range(3, int(sheets.max_column)):
-        ch_range = get_column_letter(col) + str(row+3)
-        if sheets[ch_range].value in marks_str:
-            sheets[ch_range].value = int(sheets[ch_range].value)
+    sheets['CK2'].value = 'Количество пропусков'
+    sheets.column_dimensions['CK'].width = 11
 
-# добавляем колонку КОЛИЧЕСТВО ОЦЕНОК и КОЛИЧЕСТВО ПРОПУСКОВ и подсчитываем количество оценок и пропусков по каждому учащемуся
-sheets['CJ2'].value = 'Количество оценок'
-sheets.column_dimensions['CJ'].width = 11
+    i = 3
+    for col in sheets.iter_rows(min_col=3, max_col=87, min_row=3, max_row=students_count-1):
+        marks_count = 0
+        n_count = 0
+        for cell in col:
+            if cell.value in marks_int:
+                marks_count += 1
+            elif cell.value == 'н':
+                n_count += 1
 
-sheets['CK2'].value = 'Количество пропусков'
-sheets.column_dimensions['CK'].width = 11
+        sheets['CJ'+str(i)].value = marks_count
+        sheets['CK' + str(i)].value = n_count
+        i += 1
 
-i = 3
-for col in sheets.iter_rows(min_col=3, max_col=87, min_row=3, max_row=students_count-1):
-    marks_count = 0
-    n_count = 0
-    for cell in col:
-        if cell.value in marks_int:
-            marks_count += 1
-        elif cell.value == 'н':
-            n_count += 1
+    # Вставляем учителя и предмет
+    sheets.insert_rows(1, 3)
+    sheets['B1'] = lesson
+    sheets['B2'] = teacher
 
-    sheets['CJ'+str(i)].value = marks_count
-    sheets['CK' + str(i)].value = n_count
-    i += 1
+    # ставим дату и время проверки в ячейку B2
+    sheets['B3'] = date
 
-# Вставляем учителя и предмет
-sheets.insert_rows(1, 3)
-sheets['B1'] = lesson
-sheets['B2'] = teacher
-
-# ставим дату и время проверки в ячейку B2
-sheets['B3'] = date
+    print(sheet_name,'Проверен', date)
 
 book.save(file + ' ПРОВЕРЕНО.xlsx')
 book.close()
