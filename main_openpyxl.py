9a7b026712edff5d49c77ab8560fd85925b28a8e
Ignatov.AI <ignatov-ai@yandex.ru>
2023-11-22 09:38:27 +0300

    Перестройка проекта под OpenPyXL
    Выгрузка списка листов книги класса

--- main_openpyxl.py
+++ main_openpyxl.py
@@ -1,18 +1,21 @@
-from openpyxl import Workbook
-wb = Workbook()
-
-# grab the active worksheet
-ws = wb.active
-
-# Data can be assigned directly to cells
-ws['A1'] = 42
-
-# Rows can also be appended
-ws.append([1, 2, 3])
-
-# Python types will automatically be converted
+from openpyxl import Workbook, load_workbook
 import datetime
-ws['A2'] = datetime.datetime.now()
+
+date = datetime.datetime.now()
+
+wb_in = load_workbook('11 Я.xlsx')
+wb_out = Workbook()
+
+ws_out = wb_out.active
+
+print(wb_in.sheetnames)
+
+i = 0
+
+for sheet in wb_in:
+    print(sheet.title)
+    i += 1
+    ws_out['A'+str(i)].value = sheet.title
 
 # Save the file
-wb.save("test_out.xlsx")
+wb_out.save("test_out.xlsx")
