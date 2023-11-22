6484cf2138c75b05a366d68f887ca8d14a5a173c
Ignatov.AI <ignatov-ai@yandex.ru>
2023-11-22 11:19:58 +0300

    Перестройка проекта под OpenPyXL
    Оценки каждого ученика отображаются единой строкой за весь период

--- main_openpyxl.py
+++ main_openpyxl.py
@@ -1,21 +1,42 @@
-from openpyxl import Workbook, load_workbook
+from openpyxl import load_workbook
+from openpyxl.utils import get_column_letter
+
+
 import datetime
 
 date = datetime.datetime.now()
 
-wb_in = load_workbook('11 Я.xlsx')
-wb_out = Workbook()
+book = load_workbook("11 Я тест.xlsx")
+sheets = book.active
 
-ws_out = wb_out.active
+# удаляем столбцы с ДЗ
+sheets.delete_cols(20, 23)
 
-print(wb_in.sheetnames)
+# получаем список объединенных диапазонов и разъединяем объединенные ячейки
+merged_cells = list(map(str, sheets.merged_cells.ranges))
 
-i = 0
+for item in merged_cells:
+    try:
+        sheets.unmerge_cells(item)
+    except KeyError:
+        continue
 
-for sheet in wb_in:
-    print(sheet.title)
+# перемещаем куски оценок в одну общую часть
+for part in range(1, 6):
+    m_range = 'C'+str(1+part*50)+':S'+str((part+1)*50)
+    sheets.move_range(m_range, rows=-50*part, cols=17*part)
+
+# считаем количество учеников на листе
+i = 1
+while sheets['A'+str(i)].value != '':
     i += 1
-    ws_out['A'+str(i)].value = sheet.title
 
-# Save the file
-wb_out.save("test_out.xlsx")
+# удаляем пустые строки
+sheets.delete_rows(i, sheets.max_row)
+
+# зменяем ширину всех столбцов с оценками
+for i in range(3, int(sheets.max_column)):
+    sheets.column_dimensions[get_column_letter(i)].width = 2
+
+book.save("primer2.xlsx")
+book.close()
